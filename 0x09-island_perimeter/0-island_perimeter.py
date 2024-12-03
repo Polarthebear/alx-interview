9d639c1421632e611603567a8d43a5f1b9b886c1
Polarthebear <thatotae325@gmail.com>
2024-12-03 13:50:34 +0000

    Mock Interview.

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island in the grid.
+
+    Returns:
+    int: Perimeter of the island.
+    """
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 1
+                if r < rows - 1 and grid[r + 1][c] == 1:
+                    perimeter -= 1
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 1
+                if c < cols - 1 and grid[r][c + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
